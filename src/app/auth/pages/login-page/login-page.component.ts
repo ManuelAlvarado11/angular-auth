5ce3ebb849ec77e8136f55dd1e061d739e5a5ce3
Manuel Alvarado <manuel.alvarado@bcr.gob.sv>
2024-06-11 15:48:26 -0600

    install sweet alert 2 for messges backend

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,34 +1,33 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
-
   // Properties
   private fb = inject(FormBuilder);
-  private authService = inject(AuthService)
+  private authService = inject(AuthService);
 
   public myForm: FormGroup = this.fb.group({
     email: ['manuel@mail.com', [Validators.required, Validators.email]],
     password: ['123456', [Validators.required, Validators.minLength(6)]],
-  })
+  });
 
   //Methods
-  public login(){
-    const { email, password} = this.myForm.value
+  public login() {
+    const { email, password } = this.myForm.value;
     this.authService.login(email, password).subscribe({
       next: () => {
-        console.log('Todo bien');
+        Swal.fire('Exito', '', 'success');
       },
       error: (err) => {
-        console.log({err});
-      }
-    })
+        Swal.fire('Error', err, 'error');
+      },
+    });
   }
-
 }
