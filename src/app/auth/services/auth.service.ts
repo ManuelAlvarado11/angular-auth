6cf7d84dbfc592f3552e375ce2c78bdbd4d549c7
Manuel Alvarado <manuel.alvarado@bcr.gob.sv>
2024-06-11 14:26:12 -0600

    add service in auth module

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
+import { environment } from 'src/environments/environments';
+import { AuthStatus, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  // Properties
+  private readonly baseUrl: string = environment.baseUrl
+  private http = inject(HttpClient)
+  private _currentUser = signal<User|null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
+  // Constructor
   constructor() { }
 }
